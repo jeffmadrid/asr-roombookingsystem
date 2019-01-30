98989172d45ea5bbbd3e7c8d009139db11cdef9a
jeffmadrid <jefferson.imadrid@gmail.com>
2019-01-30 14:32:30 +1100

    Generated SlotManager and SlotController

---
+++ AsrWebAPI/Data/DataManager/SlotManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Asr.Data;
+
+namespace AsrWebAPI.Data.DataManager
+{
+    public class SlotManager
+    {
+        private ApplicationDbContext _context;
+
+        public SlotManager(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
