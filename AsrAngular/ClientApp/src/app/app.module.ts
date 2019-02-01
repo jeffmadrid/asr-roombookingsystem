03871a3c77a8f00d751a1516a2d1403f925830f7
jeffmadrid <jefferson.imadrid@gmail.com>
2019-02-01 22:04:58 +1100

    Removed all things with employee -- not Assignment2

--- AsrAngular/ClientApp/src/app/app.module.ts
+++ AsrAngular/ClientApp/src/app/app.module.ts
@@ -12,10 +12,6 @@ import { HomeComponent } from "./home/home.component";
 import { CounterComponent } from "./counter/counter.component";
 import { FetchDataComponent } from "./fetch-data/fetch-data.component";
 
-import { EmployeeService } from "./services/employee.service";
-import { AddEmployeeComponent } from "./components/add-employee/add-employee.component";
-import { FetchEmployeeComponent } from "./components/fetch-employee/fetch-employee.component";
-
 import { SlotService } from "./Services/slot.service";
 import { AddRoomComponent } from './Components/add-room/add-room.component';
 import { FetchSlotComponent } from './Components/fetch-slot/fetch-slot.component';
@@ -28,8 +24,6 @@ import { FetchRoomComponent } from './Components/fetch-room/fetch-room.component
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    AddEmployeeComponent,
-    FetchEmployeeComponent,
     AddRoomComponent,
     FetchSlotComponent,
     FetchRoomComponent
@@ -45,16 +39,13 @@ import { FetchRoomComponent } from './Components/fetch-room/fetch-room.component
       { path: "", component: HomeComponent, pathMatch: "full" },
       { path: "counter", component: CounterComponent },
       { path: "fetch-data", component: FetchDataComponent },
-      { path: "fetch-employee", component: FetchEmployeeComponent },
-      { path: "add-employee", component: AddEmployeeComponent },
-      { path: "employee/edit/:id", component: AddEmployeeComponent },
       { path: "add-room", component: AddRoomComponent },
       { path: "fetch-slot", component: FetchSlotComponent },
       { path: "fetch-room", component: FetchRoomComponent }
 
     ])
   ],
-  providers: [EmployeeService, SlotService],
+  providers: [SlotService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
